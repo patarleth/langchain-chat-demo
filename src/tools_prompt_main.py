c57564a9cee8d72daf480b826171fee200fd215a
Pat Arleth <patarleth@gmail.com>
2025-02-15 21:50:35 -0500

    comments on llama3 error with tools

--- src/tools_prompt_main.py
+++ src/tools_prompt_main.py
@@ -8,9 +8,17 @@ from langchain_ollama import ChatOllama
 
 base_url=os.environ.get('OLLAMA_HOST', 'http://localhost:11434')
 
+# quick warning about meta's llama3.2 failing tool integration out of the box and returns this error -
+#
+# langchain_core.exceptions.OutputParserException: Could not parse LLM output: `Action: get_now`
+# For troubleshooting, visit: https://python.langchain.com/docs/troubleshooting/errors/OUTPUT_PARSING_FAILURE 
+# openhermes and mistral both work as is
+
 model = ChatOllama(
-    # busted out of the box - model="llama3.2:latest",
-    # fastest - model="openhermes",
+    # busted
+    # model = "llama3.2:latest",
+    # fastest - 
+    # model="openhermes",
     model = "mistral-small:latest",
     temperature=0,
     base_url=base_url,
